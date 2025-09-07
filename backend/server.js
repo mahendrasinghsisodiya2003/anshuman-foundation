7e5c81fcaf098d1b27530b83d790033117ffd674
mahendrasinghsisodiya2003 <vssisodiya2001@gmail.com>
2025-09-07 17:40:16 +0530

    completed backend

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.CLIENT_URL.trim(), // remove extra spaces
+  origin: "*",
   credentials: true
 }));
 
